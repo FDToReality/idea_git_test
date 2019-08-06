f8ab0da1a0ef6f58c9deaabf2975da763095bfef
JethroLee <731392670@qq.com>
2019-08-06 22:27:17 +0800

    添加test工程到git

---
+++ idea_git_test/src/main/java/cn/itcast/GitIdea.java
@@ -0,0 +1,13 @@
+package cn.itcast;
+
+/**
+ * @author Created by JethroLee
+ * @version v1.0
+ */
+public class GitIdea {
+    public static void main(String[] args) {
+        System.out.println("Git-Idea-Test1");
+        System.out.println("Git-Idea-Test2");
+        System.out.println("Git-Idea-Test3");
+    }
+}
